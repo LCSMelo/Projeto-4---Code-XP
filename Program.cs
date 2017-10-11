6c6169320aac9d2ddbaa6ba2e58bab6d3d0f3afe
FIC <lucas.lljm@gmail.com>
2017-10-10 21:42:51 -0300

    projeto streamWriter

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Projeto_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            FileInfo fi = new FileInfo("Arquivo.txt");
+
+            if(fi.Exists == true)
+            {
+            Console.WriteLine("Tamanho: " + fi.Length + " Bytes");
+            Console.WriteLine("Extensão: " + fi.Extension);
+            Console.WriteLine("Local do arquivo: " + fi.DirectoryName);
+            Console.WriteLine("Criado em: " + fi.CreationTime);
+            }
+            else
+            {
+                Console.WriteLine("O arquivo não existe.");
+            }
+            StreamWriter sw = new StreamWriter("Arquivo.txt", true);
+
+            sw.WriteLine("asdfasdfasdf");
+            sw.WriteLine("asdf asdf asdf");
+
+
+            sw.Close();
+
+        }
+    }
+}
